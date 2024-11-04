0457dee6d479ca46c531277726ee5bdfcfed0347
Helen-Lin-ICD201-01 <helen.lin@student.tdsb.on.ca>
2024-11-04 14:52:37 +0000

    jh

--- PP1_3.py
+++ PP1_3.py
@@ -8,18 +8,18 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-  print (" * * * * * * \n* * * * * * \n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *")
-  print ("* * * * *\n*       *\n*       *\n*       *\n* * * * *")
-  print ("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
+  print(" * * * * * * \n* * * * * * \n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *")
+  print("* * * * *\n*       *\n*       *\n*       *\n* * * * *")
+  print("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
 
 def q2():
   #Write Assignment code here
-  print ("Hello ", end = "")
-  print ("World")
+  print("Hello ", end = "")
+  print("World")
 
 def q3():
   #Write Assignment code here
-  print ('"Man"\n"Board"\n"Man Overboard"')
+  print('"Man"\n"Board"\n"Man Overboard"')
 
 #Do not alter anything after this line
 #q1()
